e9351ecf9ce403052c731e9c412163ad16a96174
Sagema <avgoustakism@yahoo.gr>
2024-12-06 18:52:33 +0200

    -m second commit

--- todo.js
+++ todo.js
@@ -1,1 +1,23 @@
-console.log(Welcome to the ToDo list!);
+console.log('Welcome to the ToDo list!');
+
+const toDoList = [];
+function addAtoDo(task) {
+    toDoList.push(task);
+    console.log('Task Added!');
+}
+
+const viewTasks = () => {
+    if (toDoList.length === 0) {
+        console.log('No tasks in your To Do List!');
+    } else {
+        for (let i = 0; i < toDoList.length; i++) {
+            console.log(i+1 + '. ' + toDoList[i]);
+        }
+    }
+}
+
+addAtoDo('Go for a 10.000 step walk');
+addAtoDo('clean the kitchen');
+addAtoDo('track your calories');
+
+viewTasks();
